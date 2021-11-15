df6f6d153196f4182256c8b87add158a1486779b
deniz_irgin@yahoo.com <deniz_irgin@yahoo.com>
2021-11-15 13:37:38 +0300

    refactor with C# 10 features

--- build/LocalStack.Build/BuildContext.cs
+++ build/LocalStack.Build/BuildContext.cs
@@ -1,166 +1,168 @@
-﻿using Cake.Common;
-using Cake.Common.IO;
-using Cake.Common.IO.Paths;
-using Cake.Common.Tools.NuGet;
-using Cake.Common.Tools.NuGet.Install;
-using Cake.Core;
-using Cake.Core.IO;
-using Cake.Frosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿namespace LocalStack.Build;
 
-namespace LocalStack.Build
+public sealed class BuildContext : FrostingContext
 {
-    public sealed class BuildContext : FrostingContext
+    public BuildContext(ICakeContext context) : base(context)
     {
-        public BuildContext(ICakeContext context) : base(context)
+        BuildConfiguration = context.Argument("config", "Release");
+        ForceBuild = context.Argument("force-build", false);
+        ForceRestore = context.Argument("force-restore", false);
+        BuildNumber = context.Argument<int>("buildnumber", 1);
+        SkipFunctionalTest = context.Argument("skipFunctionalTest", "1");
+
+        SolutionRoot = context.Directory("../../");
+        SrcPath = SolutionRoot + context.Directory("src");
+        TestsPath = SolutionRoot + context.Directory("tests");
+        BuildPath = SolutionRoot + context.Directory("build");
+        ArtifactOutput = SolutionRoot + context.Directory("artifacts");
+        ArtifactExtensionsOutput = SolutionRoot + context.Directory("artifacts-extensions");
+        LocalStackClientFolder = SrcPath + context.Directory("LocalStack.Client");
+        LocalStackClientExtFolder = SrcPath + context.Directory("LocalStack.Client.Extensions");
+
+        SlnFilePath = SolutionRoot + context.File("LocalStack.sln");
+        LocalStackClientProjFile = LocalStackClientFolder + context.File("LocalStack.Client.csproj");
+        LocalStackClientExtProjFile = LocalStackClientExtFolder + context.File("LocalStack.Client.Extensions.csproj");
+    }
+
+    public string BuildConfiguration { get; }
+
+    public bool ForceBuild { get; }
+
+    public bool ForceRestore { get; }
+
+    public string TestingMode { get; }
+
+    public string SkipFunctionalTest { get; set; }
+
+    public int BuildNumber { get; set; }
+
+    public ConvertableFilePath SlnFilePath { get; set; }
+
+    public ConvertableDirectoryPath SolutionRoot { get; }
+
+    public ConvertableDirectoryPath SrcPath { get; }
+
+    public ConvertableDirectoryPath TestsPath { get; }
+
+    public ConvertableDirectoryPath BuildPath { get; }
+
+    public ConvertableDirectoryPath ArtifactOutput { get; set; }
+
+    public ConvertableDirectoryPath ArtifactExtensionsOutput { get; set; }
+
+    public ConvertableDirectoryPath LocalStackClientFolder { get; set; }
+
+    public ConvertableDirectoryPath LocalStackClientExtFolder { get; set; }
+
+    public ConvertableFilePath LocalStackClientProjFile { get; set; }
+
+    public ConvertableFilePath LocalStackClientExtProjFile { get; set; }
+
+    public void InstallXUnitNugetPackage()
+    {
+        var nugetInstallSettings = new NuGetInstallSettings
         {
-            BuildConfiguration = context.Argument("config", "Release");
-            ForceBuild = context.Argument("force-build", false);
-            ForceRestore = context.Argument("force-restore", false);
-            BuildNumber = context.Argument<int>("buildnumber", 1);
-            SkipFunctionalTest = context.Argument("skipFunctionalTest", "1");
+            Version = "2.4.1",
+            Verbosity = NuGetVerbosity.Normal,
+            OutputDirectory = "testrunner",
+            WorkingDirectory = "."
+        };
 
-            SolutionRoot = context.Directory("../../");
-            SrcPath = SolutionRoot + context.Directory("src");
-            TestsPath = SolutionRoot + context.Directory("tests");
-            BuildPath = SolutionRoot + context.Directory("build");
-            ArtifactOutput = SolutionRoot + context.Directory("artifacts");
-            ArtifactExtensionsOutput = SolutionRoot + context.Directory("artifacts-extensions");
-            LocalStackClientFolder = SrcPath + context.Directory("LocalStack.Client");
-            LocalStackClientExtFolder = SrcPath + context.Directory("LocalStack.Client.Extensions");
+        this.NuGetInstall("xunit.runner.console", nugetInstallSettings);
+    }
 
-            SlnFilePath = SolutionRoot + context.File("LocalStack.sln");
-            LocalStackClientProjFile = LocalStackClientFolder + context.File("LocalStack.Client.csproj");
-            LocalStackClientExtProjFile = LocalStackClientExtFolder + context.File("LocalStack.Client.Extensions.csproj");
+    public IList<ProjMetadata> GetProjMetadata()
+    {
+        DirectoryPath testsRoot = this.MakeAbsolute(this.Directory(TestsPath));
+        List<FilePath> csProjFile = this.GetFiles($"{testsRoot}/**/*.csproj").Where(fp => fp.FullPath.EndsWith("Tests.csproj")).ToList();
+
+        IList<ProjMetadata> projMetadata = new List<ProjMetadata>();
+
+        foreach (FilePath csProj in csProjFile)
+        {
+            string csProjPath = csProj.FullPath;
+
+            string[] targetFrameworks = this.GetProjectTargetFrameworks(csProjPath);
+            string directoryPath = csProj.GetDirectory().FullPath;
+            string assemblyName = GetAssemblyName(csProjPath);
+
+            var testProjMetadata = new ProjMetadata(directoryPath, csProjPath, targetFrameworks, assemblyName);
+            projMetadata.Add(testProjMetadata);
         }
 
-        public string BuildConfiguration { get; }
+        return projMetadata;
+    }
 
-        public bool ForceBuild { get; }
+    public string[] GetProjectTargetFrameworks(string csprojPath)
+    {
+        FilePath file = this.MakeAbsolute(this.File(csprojPath));
+        string project = System.IO.File.ReadAllText(file.FullPath, Encoding.UTF8);
 
-        public bool ForceRestore { get; }
+        bool multipleFrameworks = project.Contains("<TargetFrameworks>");
+        string startElement = multipleFrameworks ? "<TargetFrameworks>" : "<TargetFramework>";
+        string endElement = multipleFrameworks ? "</TargetFrameworks>" : "</TargetFramework>";
 
-        public string TestingMode { get; }
+        int startIndex = project.IndexOf(startElement, StringComparison.Ordinal) + startElement.Length;
+        int endIndex = project.IndexOf(endElement, startIndex, StringComparison.Ordinal);
 
-        public string SkipFunctionalTest { get; set; }
+        string targetFrameworks = project.Substring(startIndex, endIndex - startIndex);
 
-        public int BuildNumber { get; set; }
+        return targetFrameworks.Split(';');
+    }
 
-        public ConvertableFilePath SlnFilePath { get; set; }
+    public string GetAssemblyName(string csprojPath)
+    {
+        FilePath file = this.MakeAbsolute(this.File(csprojPath));
+        string project = System.IO.File.ReadAllText(file.FullPath, Encoding.UTF8);
 
-        public ConvertableDirectoryPath SolutionRoot { get; }
+        bool assemblyNameElementExists = project.Contains("<AssemblyName>");
 
-        public ConvertableDirectoryPath SrcPath { get; }
+        string assemblyName;
 
-        public ConvertableDirectoryPath TestsPath { get; }
-
-        public ConvertableDirectoryPath BuildPath { get; }
-
-        public ConvertableDirectoryPath ArtifactOutput { get; set; }
-
-        public ConvertableDirectoryPath ArtifactExtensionsOutput { get; set; }
-
-        public ConvertableDirectoryPath LocalStackClientFolder { get; set; }
-
-        public ConvertableDirectoryPath LocalStackClientExtFolder { get; set; }
-
-        public ConvertableFilePath LocalStackClientProjFile { get; set; }
-
-        public ConvertableFilePath LocalStackClientExtProjFile { get; set; }
-
-        public void InstallXUnitNugetPackage()
+        if (assemblyNameElementExists)
         {
-            var nugetInstallSettings = new NuGetInstallSettings
-            {
-                Version = "2.4.1",
-                Verbosity = NuGetVerbosity.Normal,
-                OutputDirectory = "testrunner",
-                WorkingDirectory = "."
-            };
+            int startIndex = project.IndexOf("<AssemblyName>", StringComparison.Ordinal) + "<AssemblyName>".Length;
+            int endIndex = project.IndexOf("</AssemblyName>", startIndex, StringComparison.Ordinal);
 
-            this.NuGetInstall("xunit.runner.console", nugetInstallSettings);
+            assemblyName = project.Substring(startIndex, endIndex - startIndex);
+        }
+        else
+        {
+            int startIndex = csprojPath.LastIndexOf("/", StringComparison.Ordinal) + 1;
+            int endIndex = csprojPath.IndexOf(".csproj", startIndex, StringComparison.Ordinal);
+
+            assemblyName = csprojPath.Substring(startIndex, endIndex - startIndex);
         }
 
-        public IList<TestProjMetadata> GetProjMetadata()
+        return assemblyName;
+    }
+
+    public void RunXunitUsingMono(string targetFramework, string assemblyPath)
+    {
+        int exitCode = this.StartProcess("mono", new ProcessSettings
         {
-            DirectoryPath testsRoot = this.MakeAbsolute(this.Directory(TestsPath));
-            List<FilePath> csProjs = this.GetFiles($"{testsRoot}/**/*.csproj").Where(fp => fp.FullPath.EndsWith("Tests.csproj")).ToList();
+            Arguments = $"./testrunner/xunit.runner.console.2.4.1/tools/{targetFramework}/xunit.console.exe {assemblyPath}"
+        });
 
-            IList<TestProjMetadata> testProjMetadatas = new List<TestProjMetadata>();
-
-            foreach (var csProj in csProjs)
-            {
-                string csProjPath = csProj.FullPath;
-
-                string[] targetFrameworks = this.GetProjectTargetFrameworks(csProjPath);
-                string directoryPath = csProj.GetDirectory().FullPath;
-                string assemblyName = GetAssemblyName(csProjPath);
-
-                var testProjMetadata = new TestProjMetadata(directoryPath, csProjPath, targetFrameworks, assemblyName);
-                testProjMetadatas.Add(testProjMetadata);
-            }
-
-            return testProjMetadatas;
-        }
-
-        public string[] GetProjectTargetFrameworks(string csprojPath)
+        if (exitCode != 0)
         {
-            FilePath file = this.MakeAbsolute(this.File(csprojPath));
-            string project = System.IO.File.ReadAllText(file.FullPath, Encoding.UTF8);
-
-            bool multipleFrameworks = project.Contains("<TargetFrameworks>");
-            string startElement = multipleFrameworks ? "<TargetFrameworks>" : "<TargetFramework>";
-            string endElement = multipleFrameworks ? "</TargetFrameworks>" : "</TargetFramework>";
-
-            int startIndex = project.IndexOf(startElement, StringComparison.Ordinal) + startElement.Length;
-            int endIndex = project.IndexOf(endElement, startIndex, StringComparison.Ordinal);
-
-            string targetFrameworks = project.Substring(startIndex, endIndex - startIndex);
-
-            return targetFrameworks.Split(';');
+            throw new InvalidOperationException($"Exit code: {exitCode}");
         }
+    }
 
-        public string GetAssemblyName(string csprojPath)
-        {
-            FilePath file = this.MakeAbsolute(this.File(csprojPath));
-            string project = System.IO.File.ReadAllText(file.FullPath, Encoding.UTF8);
+    public string GetProjectVersion()
+    {
+        FilePath file =  this.MakeAbsolute(this.File("./src/Directory.Build.props"));
 
-            bool assemblyNameElementExists = project.Contains("<AssemblyName>");
+        this.Information(file.FullPath);
 
-            string assemblyName;
+        string project = System.IO.File.ReadAllText(file.FullPath, Encoding.UTF8);
+        int startIndex = project.IndexOf("<Version>", StringComparison.Ordinal) + "<Version>".Length;
+        int endIndex = project.IndexOf("</Version>", startIndex, StringComparison.Ordinal);
 
-            if (assemblyNameElementExists)
-            {
-                int startIndex = project.IndexOf("<AssemblyName>", StringComparison.Ordinal) + "<AssemblyName>".Length;
-                int endIndex = project.IndexOf("</AssemblyName>", startIndex, StringComparison.Ordinal);
+        string version = project.Substring(startIndex, endIndex - startIndex);
+        version = $"{version}.{BuildNumber}";
 
-                assemblyName = project.Substring(startIndex, endIndex - startIndex);
-            }
-            else
-            {
-                int startIndex = csprojPath.LastIndexOf("/", StringComparison.Ordinal) + 1;
-                int endIndex = csprojPath.IndexOf(".csproj", startIndex, StringComparison.Ordinal);
-
-                assemblyName = csprojPath.Substring(startIndex, endIndex - startIndex);
-            }
-
-            return assemblyName;
-        }
-
-        public void RunXunitUsingMono(string targetFramework, string assemblyPath)
-        {
-            int exitCode = this.StartProcess("mono", new ProcessSettings
-            {
-                Arguments = $"./testrunner/xunit.runner.console.2.4.1/tools/{targetFramework}/xunit.console.exe {assemblyPath}"
-            });
-
-            if (exitCode != 0)
-            {
-                throw new InvalidOperationException($"Exit code: {exitCode}");
-            }
-        }
+        return version;
     }
 }
--- build/LocalStack.Build/Program.cs
+++ build/LocalStack.Build/Program.cs
@@ -1,144 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
+﻿return new CakeHost()
+       .UseContext<BuildContext>()
+       .Run(args);
 
-using Cake.Common;
-using Cake.Common.Diagnostics;
-using Cake.Common.Tools.DotNetCore;
-using Cake.Common.Tools.DotNetCore.Build;
-using Cake.Common.Tools.DotNetCore.MSBuild;
-using Cake.Common.Tools.DotNetCore.Pack;
-using Cake.Common.Tools.DotNetCore.Test;
-using Cake.Core;
-using Cake.Core.IO;
-using Cake.Frosting;
-
-namespace LocalStack.Build
+[TaskName("Default"),
+ IsDependentOn(typeof(InitTask)),
+ IsDependentOn(typeof(TestTask))]
+public class DefaultTask : FrostingTask
 {
-    internal static class Program
+}
+
+[TaskName("init")]
+public sealed class InitTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
     {
-        private static int Main(string[] args)
+        context.StartProcess("dotnet", new ProcessSettings
         {
-            return new CakeHost()
-                   .UseContext<BuildContext>()
-                   .Run(args);
+            Arguments = "--info"
+        });
+
+        if (!context.IsRunningOnUnix())
+        {
+            return;
         }
 
-        [TaskName("Default"),
-         IsDependentOn(typeof(InitTask)),
-         IsDependentOn(typeof(TestTask))]
-        public class DefaultTask : FrostingTask
+        context.StartProcess("git", new ProcessSettings
         {
-        }
+            Arguments = "config --global core.autocrlf true"
+        });
 
-        [TaskName("init")]
-        public sealed class InitTask : FrostingTask<BuildContext>
+        context.StartProcess("mono", new ProcessSettings
         {
-            public override void Run(BuildContext context)
+            Arguments = "--version"
+        });
+
+        context.InstallXUnitNugetPackage();
+    }
+}
+
+[TaskName("build")]
+public sealed class BuildTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetCoreBuild(context.SlnFilePath,
+                                new DotNetCoreBuildSettings
+                                {
+                                    Configuration = context.BuildConfiguration
+                                });
+    }
+}
+
+[TaskName("test"),
+ IsDependentOn(typeof(BuildTask))]
+public sealed class TestTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        const string testResults = "results.trx";
+
+        var settings = new DotNetCoreTestSettings
+        {
+            NoRestore = !context.ForceRestore,
+            NoBuild = !context.ForceBuild,
+            Configuration = context.BuildConfiguration
+        };
+
+        IList<ProjMetadata> projMetadata = context.GetProjMetadata();
+
+        foreach (ProjMetadata testProj in projMetadata)
+        {
+            string testProjectPath = testProj.CsProjPath;
+
+            context.Warning($"Target Frameworks {string.Join(" ", testProj.TargetFrameworks)}");
+
+            foreach (string targetFramework in testProj.TargetFrameworks)
             {
-                context.StartProcess("dotnet", new ProcessSettings
+                if (context.SkipFunctionalTest == "1" && testProj.AssemblyName == "LocalStack.Client.Functional.Tests")
                 {
-                    Arguments = "--info"
-                });
-
-                if (!context.IsRunningOnUnix())
-                {
-                    return;
+                    context.Warning("Skipping Functional Tests");
+                    continue;
                 }
 
-                context.StartProcess("git", new ProcessSettings
+                context.Warning($"Running {targetFramework.ToUpper()} tests for {testProj.AssemblyName}");
+                settings.Framework = targetFramework;
+
+                if (context.IsRunningOnUnix() && targetFramework == "net461")
                 {
-                    Arguments = "config --global core.autocrlf true"
-                });
-
-                context.StartProcess("mono", new ProcessSettings
+                    context.RunXunitUsingMono(targetFramework, $"{testProj.DirectoryPath}/bin/{context.BuildConfiguration}/{targetFramework}/{testProj.AssemblyName}.dll");
+                }
+                else
                 {
-                    Arguments = "--version"
-                });
-
-                context.InstallXUnitNugetPackage();
-            }
-        }
-
-        [TaskName("build")]
-        public sealed class BuildTask : FrostingTask<BuildContext>
-        {
-            public override void Run(BuildContext context)
-            {
-                context.DotNetCoreBuild(context.SlnFilePath,
-                                        new DotNetCoreBuildSettings
-                                        {
-                                            Configuration = context.BuildConfiguration
-                                        });
-            }
-        }
-
-        [TaskName("test"),
-         IsDependentOn(typeof(BuildTask))]
-        public sealed class TestTask : FrostingTask<BuildContext>
-        {
-            public override void Run(BuildContext context)
-            {
-                var testResults = "results.trx";
-
-                var settings = new DotNetCoreTestSettings
-                {
-                    NoRestore = !context.ForceRestore,
-                    NoBuild = !context.ForceBuild,
-                    Configuration = context.BuildConfiguration
-                };
-
-                IList<TestProjMetadata> testProjMetadatas = context.GetProjMetadata();
-
-                foreach (var testProj in testProjMetadatas)
-                {
-                    string testProjectPath = testProj.CsProjPath;
-
-                    context.Warning($"Target Frameworks {string.Join(" ", testProj.TargetFrameworks)}");
-
-                    foreach (string targetFramework in testProj.TargetFrameworks)
-                    {
-                        if (context.SkipFunctionalTest == "1" && testProj.AssemblyName == "LocalStack.Client.Functional.Tests")
-                        {
-                            context.Warning("Skipping Functional Tests");
-                            continue;
-                        }
-
-                        context.Warning($"Running {targetFramework.ToUpper()} tests for {testProj.AssemblyName}");
-                        settings.Framework = targetFramework;
-
-                        if (context.IsRunningOnUnix() && targetFramework == "net461")
-                        {
-                            context.RunXunitUsingMono(targetFramework, $"{testProj.DirectoryPath}/bin/{context.BuildConfiguration}/{targetFramework}/{testProj.AssemblyName}.dll");
-                        }
-                        else
-                        {
-                            string testFilePrefix = targetFramework.Replace(".", "-");
-                            settings.ArgumentCustomization = args => args.Append($" --logger \"trx;LogFileName={testFilePrefix}_{testResults}\"");
-                            context.DotNetCoreTest(testProjectPath, settings);
-                        }
-                    }
+                    string testFilePrefix = targetFramework.Replace(".", "-");
+                    settings.ArgumentCustomization = args => args.Append($" --logger \"trx;LogFileName={testFilePrefix}_{testResults}\"");
+                    context.DotNetCoreTest(testProjectPath, settings);
                 }
             }
         }
+    }
+}
 
-        [TaskName("nuget-pack")]
-        public sealed class NugetPackTask : FrostingTask<BuildContext>
+[TaskName("nuget-pack")]
+public sealed class NugetPackTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        if (!System.IO.Directory.Exists(context.ArtifactOutput))
         {
-            public override void Run(BuildContext context)
-            {
-                if (!System.IO.Directory.Exists(context.ArtifactOutput))
-                {
-                    System.IO.Directory.CreateDirectory(context.ArtifactOutput);
-                }
-
-                var settings = new DotNetCorePackSettings();
-                settings.Configuration = context.BuildConfiguration;
-                settings.OutputDirectory = context.ArtifactOutput;
-                settings.MSBuildSettings = new DotNetCoreMSBuildSettings();
-                settings.MSBuildSettings.SetVersion(GetProjectVersion());
-
-                context.DotNetCorePack(projectFullPath, settings);
-            }
+            System.IO.Directory.CreateDirectory(context.ArtifactOutput);
         }
+
+        var settings = new DotNetCorePackSettings
+        {
+            Configuration = context.BuildConfiguration, 
+            OutputDirectory = context.ArtifactOutput, 
+            MSBuildSettings = new DotNetCoreMSBuildSettings()
+        };
+
+        settings.MSBuildSettings.SetVersion(context.GetProjectVersion());
+
+        context.DotNetCorePack(context.LocalStackClientProjFile, settings);
     }
 }

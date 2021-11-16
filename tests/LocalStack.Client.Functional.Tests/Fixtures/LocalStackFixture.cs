a79b5f9d482b15fdc6d9db251726400c1aacdbb8
deniz_irgin@yahoo.com <deniz_irgin@yahoo.com>
2021-11-16 19:57:20 +0300

    update localstack container to 0.13.0

--- tests/LocalStack.Client.Functional.Tests/Fixtures/LocalStackFixture.cs
+++ tests/LocalStack.Client.Functional.Tests/Fixtures/LocalStackFixture.cs
@@ -1,39 +1,30 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿namespace LocalStack.Client.Functional.Tests.Fixtures;
 
-using DotNet.Testcontainers.Containers.Builders;
-using DotNet.Testcontainers.Containers.Modules;
-
-using Xunit;
-
-namespace LocalStack.Client.Functional.Tests.Fixtures
+public class LocalStackFixture : IAsyncLifetime
 {
-    public class LocalStackFixture : IAsyncLifetime
+    private readonly TestcontainersContainer _localStackContainer;
+
+    public LocalStackFixture()
     {
-        private readonly TestcontainersContainer _localStackContainer;
+        ITestcontainersBuilder<TestcontainersContainer> localStackBuilder = new TestcontainersBuilder<TestcontainersContainer>()
+            .WithName($"LocalStack-0.13.0-{DateTime.Now.Ticks}")
+            .WithImage("localstack/localstack:0.13.0")
+            .WithCleanUp(true)
+            .WithEnvironment("DEFAULT_REGION", "eu-central-1")
+            .WithEnvironment("SERVICES", "s3,dynamodb,sqs")
+            .WithEnvironment("DOCKER_HOST", "unix:///var/run/docker.sock")
+            .WithEnvironment("LS_LOG", "info")
+            .WithPortBinding(4566, 4566);
 
-        public LocalStackFixture()
-        {
-            ITestcontainersBuilder<TestcontainersContainer> localStackBuilder = new TestcontainersBuilder<TestcontainersContainer>()
-                .WithName($"LocalStack-0.12.20-{DateTime.Now.Ticks}")
-                .WithImage("localstack/localstack:0.12.20")
-                .WithCleanUp(true)
-                .WithEnvironment("DEFAULT_REGION", "eu-central-1")
-                .WithEnvironment("SERVICES", "s3,dynamodb,sqs")
-                .WithEnvironment("DOCKER_HOST", "unix:///var/run/docker.sock")
-                .WithEnvironment("LS_LOG", "info")
-                .WithPortBinding(4566, 4566);
+        _localStackContainer = localStackBuilder.Build();
+    }
+    public async Task InitializeAsync()
+    {
+        await _localStackContainer.StartAsync();
+    }
 
-            _localStackContainer = localStackBuilder.Build();
-        }
-        public async Task InitializeAsync()
-        {
-            await _localStackContainer.StartAsync();
-        }
-
-        public async Task DisposeAsync()
-        {
-            await _localStackContainer.StopAsync();
-        }
+    public async Task DisposeAsync()
+    {
+        await _localStackContainer.StopAsync();
     }
 }

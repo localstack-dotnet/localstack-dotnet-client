56be5f6c099ad6dcd417be58c87bb759f6c3466d
deniz_irgin@yahoo.com <deniz_irgin@yahoo.com>
2023-01-29 20:00:05 +0300

    fix ServiceUrl related test. See 1.4.0 release notes

--- tests/LocalStack.Client.Tests/Mocks/MockServiceClients/MockAwsServiceEndpoint.cs
+++ tests/LocalStack.Client.Tests/Mocks/MockServiceClients/MockAwsServiceEndpoint.cs
@@ -1,5 +1,5 @@
 ﻿namespace LocalStack.Client.Tests.Mocks.MockServiceClients;
 
 public record MockAwsServiceEndpoint() 
-    : AwsServiceEndpoint(MockServiceMetadata.MockServiceId, "mockService", AwsServiceEnum.ApiGateway, 1234, "localhost", "http://localhost:1234");
+    : AwsServiceEndpoint(MockServiceMetadata.MockServiceId, "mockService", AwsServiceEnum.ApiGateway, 1234, "localhost", "http://localhost:1234/");
 

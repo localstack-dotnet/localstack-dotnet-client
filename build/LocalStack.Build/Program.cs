b13fcaa950b8d4c1a3ce9060b9054b898d49bedf
Blind-Striker <deniz_irgin@yahoo.com>
2024-06-02 19:44:55 +0300

    skip mono tests for net462 for linux for the time beign

--- build/LocalStack.Build/Program.cs
+++ build/LocalStack.Build/Program.cs
@@ -90,7 +90,11 @@ public sealed class TestTask : FrostingTask<BuildContext>
                     }
                 }
 
-                if (context.IsRunningOnUnix() && targetFramework == "net462")
+                if (context.IsRunningOnLinux() && targetFramework == "net462")
+                {
+                    context.Warning("Temporarily disabled running net462 tests on Linux because of a problem in mono runtime");
+                }
+                else if (context.IsRunningOnMacOs() && targetFramework == "net462")
                 {
                     context.RunXUnitUsingMono(targetFramework, $"{testProj.DirectoryPath}/bin/{context.BuildConfiguration}/{targetFramework}/{testProj.AssemblyName}.dll");
                 }
